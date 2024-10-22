f5bc05fb59484a8d16fd7551b90eb332a05fab1b
mdjamal <md.jamal@globallogic.com>
2024-10-22 16:01:02 +0530

    comment added

--- palindromeString.cpp
+++ palindromeString.cpp
@@ -4,10 +4,13 @@
 
 #include <iostream>
 
+//check palindrome function
 bool checkPalindrome(std::string &str)
 {
     int stringLength = str.length();
     
+    //check the equality of first element with last, vice versa
+    //loop runs till the middle of the string
     for(int i = 0 ; i < stringLength/2 ; i++)
     {
         if(str[i] != str[stringLength - 1 - i])
